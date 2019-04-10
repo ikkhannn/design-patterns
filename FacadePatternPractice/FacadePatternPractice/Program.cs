7a15dc2123730a035d339074a24a2f71f6f364c9
ikkhannn <imrankhannn81@gmail.com>
2019-04-11 00:07:53 +0500

    Facade pattern

---
+++ FacadePatternPractice/FacadePatternPractice/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FacadePatternPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Facade facade = new Facade();
+            facade.completeCar();
+            Console.ReadKey();
+        }
+    }
+
+    public class Engine
+    {
+       public void setEngine()
+        {
+            Console.WriteLine("Engine");
+        }
+    }
+    
+    public class Body
+    {
+        public void setBody() {
+            Console.WriteLine("Body");
+        }
+    }
+
+    public class Accessories
+    {
+        public void setAccessories()
+        {
+            Console.WriteLine("Accessories");
+        }
+    }
+
+
+
+    public class Facade
+    {
+
+        public void completeCar()
+        {
+            Accessories accessories = new Accessories();
+            Body body = new Body();
+            Engine engine = new Engine();
+
+            accessories.setAccessories();
+            body.setBody();
+            engine.setEngine();
+        }
+
+
+
+    }
+
+}
+
+
